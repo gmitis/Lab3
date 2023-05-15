ed9f8def7743561983f29b473140da64c205e3f2
gmitis <gi.muths@gmail.com>
2023-05-15 08:34:47 +0300

    intermediate commit for pushing

--- main.py
+++ main.py
@@ -1,89 +1,93 @@
-# TODO: calculate metrics (avg customers in system)
 # TODO: debugging code
+# TODO: (debug) fix transition table: when one customer is blocked time of the rest could be wrong
+# TODO: (debug) fix simulation: values not correct
 
-import random
-import numpy as np
-import bisect
+import queue
+from utils import plot as plt
+from utils import create_transitions as ct
 
 lmd = [1, 5, 10]
 mu = 5
 seed = 42
 iters = 10000
-
-
-# calculate poisson distribution to find how many events per minute
-# calculate arrival time of x events in every minute (normal distribution is considered)
-def create_event_times(seq):
-    new_seq = []
-    minute = -1
-    random.seed(seed)
-    for i in seq:
-        minute += 1
-        x = 0
-        while x < i:
-            y = random.randint(0, 59) + 60*minute
-            index = bisect.bisect_left(new_seq, y)
-            new_seq.insert(index, y)
-            x += 1
-    return new_seq
-
-
-# creates continuous transition list with times of arrival or departure of an event
-def set_transition_table(arrivals, departures):
-    dep_times = [x+y for x, y in zip(arrivals, departures)]
-    arrivals = list(map(lambda x: ('a', x), arrivals))
-    dep_times = list(map(lambda x: ('d', x), dep_times))
-    trans = arrivals + dep_times
-    trans = list(sorted(trans, key=lambda x: x[1]))[:iters]
-    print(len(trans), trans)
-    return trans
-
-
-# create a state matrix
-def create_states(lmd, mu):
-    np.random.seed(seed)
-    seq = np.random.poisson(lmd, iters)
-    arrivals_time = create_event_times(seq)
-    seq = np.random.exponential(1/mu,  iters)
-    departures_intervals = [x*60 for x in seq]
-    transitions = set_transition_table(arrivals_time, departures_intervals)
-    return transitions
+queue_length = 10
 
 
 # debugger
-def trace(number_of_state_transfers):
-    pass
+def trace(i, qs, s, arr):
+    print(f'\nTransition: {i+1}\nSystem state: {qs}\nNext transition: {s}\nNumber of arrivals: {arr}\n')
 
 
-# ergodic state probabilities of systemn
-def stable_state_probabilities():
-    pass
-
-
-# probability of a customer being rejected cause system is full
-def blocking_probability():
-    pass
-
-
-# average number of customers in the system
-def average_customers():
-    pass
-
-
-# average time spent in the system by a customer
-def average_time():
-    pass
-
-
+# k:= index of En
+# m := index that shows if we need to check for convergence
 def simulate(trans):
+    q = queue.Queue()
+    Pn = [0] * queue_length
+    Pb = 0
+    En = [0] * (int(iters/1000))
+    Et = 0
+    qs = 0
+    iter = -1
+    empty_since = 0
+    arrivals = 0
+
     for i in trans:
-        pass
+        iter += 1
+
+        # trace code for debugging
+        if iter < 30:
+            if i[0] == 'a': arrivals += 1
+            trace(iter, q.qsize(), trans[iter+1][0], arrivals)
+
+        k = iter // 1000
+        m = iter % 1000
+
+        if i[0] == 'a' and qs < 10:
+            if qs == 0:
+                Pn[0] += i[1] - empty_since
+            q.put(i[1])
+            qs += 1
+            En[k] += qs
+        elif i[0] == 'a' and qs == 10:
+            Pb += 1
+            En[k] += qs
+        elif i[0] == 'd' and qs > 0:
+            if qs == 1:
+                empty_since = i[1]
+            wait_time = i[1] - q.get()
+            Pn[qs] = wait_time
+            qs -= 1
+            En[k] += qs
+            Et += wait_time
+
+        if m == 0:
+            En[k-1] /= 1000
+            if k>1 and abs(En[k-1]-En[k-2])/En[k-2] < 0.00001:
+                break
+
+    print(f'Pn: {Pn}\nPb: {Pb}\nEn: {En}\nEt: {Et}\n')
+    Pn = list(map(lambda x: x/trans[iters][1], Pn))
+    return Pn, Pb/iters, En, Et
 
 
 if __name__ == '__main__':
-    transitions = create_states(5, mu)
-    simulate(transitions)
+    for l in lmd:
+        Trans = ct.Transitions(seed, iters, queue_length)
+        transitions = Trans.create_states(l, mu)
 
+        # debug
+        blk =0
+        a = 0
+        for x in transitions:
+            if x[0] == 'a':
+                a += 1
+                if a >= 11:
+                    blk += 1
+            else:
+                a = 0
+        print(f'Blocked {blk} times')
+        # --debug
 
-
-
+        ergodic_prob, p_blocking, avg_customers_in_system, avg_waiting_time = simulate(transitions)
+        plt.plot_erg_prob(ergodic_prob)
+        plt.plot_avg_cust(avg_customers_in_system)
